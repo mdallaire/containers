bf503dcfdda29e12152df1c56456da0ca9c3309b
mdallaire <23340663+mdallaire@users.noreply.github.com>
2023-10-19 15:46:39 -0400

    fix version output

--- apps/waterfurnace-aurora/ci/latest.sh
+++ apps/waterfurnace-aurora/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -L -s https://rubygems.org/api/v1/versions/waterfurnace_aurora.json | jq '.[0].number' 2>/dev/null)
+version=$(curl -L -s https://rubygems.org/api/v1/versions/waterfurnace_aurora.json | jq '.[0].number' | tr -d '"' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
