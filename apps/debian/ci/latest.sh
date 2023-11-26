b736e2c609859d8d45385600b5bd5bdb1ebe9d0c
mdallaire <23340663+mdallaire@users.noreply.github.com>
2023-11-26 14:40:40 -0500

    remove slim

--- apps/debian/ci/latest.sh
+++ apps/debian/ci/latest.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 channel=$1
-if [[ $channel == *"-slim" ]]; then
-    channelslim=${channel%-slim}
-    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channelslim" | jq --raw-output --arg s "$channelslim" '.results[] | select(.name | test($s + "-[0-9]{8}-slim")) | .name' 2>/dev/null | head -n1)
-else
-    # non-slim
-    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | test($s + "-[0-9]{8}(?!-slim)")) | .name' 2>/dev/null | head -n1)
-fi
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | test($s + "-[0-9]{8}(?!-slim)")) | .name' 2>/dev/null | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
