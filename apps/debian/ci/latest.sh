05f09cf8dfa2e8e1d27a069a8b170490ffca8d94
mdallaire <23340663+mdallaire@users.noreply.github.com>
2023-11-26 13:58:52 -0500

    fix slim

--- apps/debian/ci/latest.sh
+++ apps/debian/ci/latest.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 channel=$1
 if [[ $channel == *"-slim" ]]; then
-    # slim
     channelslim=${channel%-slim}
-    echo Channelslim $channelslim
     version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channelslim" | jq --raw-output --arg s "$channelslim" '.results[] | select(.name | test($s + "-[0-9]{8}-slim")) | .name' 2>/dev/null | head -n1)
 else
     # non-slim
