67dc7276d5048fd359f8f6fc977e1d503332262c
mdallaire <23340663+mdallaire@users.noreply.github.com>
2023-11-26 13:53:56 -0500

    slim and non-slim

--- apps/debian/ci/latest.sh
+++ apps/debian/ci/latest.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 channel=$1
-version=$channel
-# version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | contains($s)) | .name' 2>/dev/null | head -n1)
-#version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name == $s) | .name' 2>/dev/null | head -n1)
-#version="${version#*v}"
-#version="${version#*release-}"
+if [[ $channel == *"-slim" ]]; then
+    # slim
+    channelslim=${channel%-slim}
+    echo Channelslim $channelslim
+    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channelslim" | jq --raw-output --arg s "$channelslim" '.results[] | select(.name | test($s + "-[0-9]{8}-slim")) | .name' 2>/dev/null | head -n1)
+else
+    # non-slim
+    version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/debian/tags?ordering=name&name=$channel" | jq --raw-output --arg s "$channel" '.results[] | select(.name | test($s + "-[0-9]{8}(?!-slim)")) | .name' 2>/dev/null | head -n1)
+fi
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
-
-
-
-#version="${version#*v}"
