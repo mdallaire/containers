de48285747bedd843d952e77de175ea6490a6495
Mathieu Dallaire <git@dlai.re>
2024-05-10 22:10:54 -0400

    remove hydroqc2mqtt

--- apps/hydroqc2mqtt/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -L -s https://pypi.python.org/pypi/hydroqc2mqtt/json | jq --raw-output '.info.version' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
