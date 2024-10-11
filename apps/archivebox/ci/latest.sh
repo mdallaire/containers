66ed6edd0e8e66b0c61fc9a09ccf58e6676125d7
Mathieu Dallaire <git@dlai.re>
2024-10-11 15:29:53 -0400

    cleanup

--- apps/archivebox/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -L -s https://pypi.python.org/pypi/parsedmarc/json | jq --raw-output '.info.version' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
