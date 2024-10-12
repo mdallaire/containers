05c1d45f6702ed526c1feb9983222ebc198bf563
Mathieu Dallaire <git@dlai.re>
2024-10-11 20:46:25 -0400

    path

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2086
 
-export PATH="$HOME/.local/bin:$PATH"
-
 if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
     curl -sfSL https://get.hacs.xyz | bash -
 fi
 
 exec \
-    /usr/local/bin/hass \
+    PATH="$HOME/.local/bin:$PATH" /usr/local/bin/hass \
         --config /config \
         "$@"
