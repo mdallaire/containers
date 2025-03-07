3f012e1d111ce22c01076bc4b79812bf98d205e0
Mathieu Dallaire <23340663+mdallaire@users.noreply.github.com>
2025-03-07 11:42:32 -0500

    Update entrypoint.sh

--- apps/bitwarden-cli/entrypoint.sh
+++ apps/bitwarden-cli/entrypoint.sh
@@ -2,23 +2,10 @@
 
 set -e
 
-# Define the data file path
-BW_DATA_FILE="/root/.config/Bitwarden CLI/data.json"
-
-# Configure server
 bw config server ${BW_HOST}
 
-# Check if data.json exists (indicating user is already logged in)
-if [ ! -f "$BW_DATA_FILE" ]; then
-    echo "Bitwarden data file not found. Logging in..."
-    bw login --apikey --raw
-    export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
-else
-    echo "Bitwarden data file found. Unlocking..."
-    export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
-fi
-
-# Verify unlock status
+bw login --apikey --raw
+export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
 bw unlock --check
 
 echo 'Running `bw server` on port 8087'
